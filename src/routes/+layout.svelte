ce1432cdf570aa4829d83d40a0bd25df4bdba013
Henrique Proscholdt <proscholdt.h2014@gmail.com>
2025-08-29 19:41:23 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import '../global.css'
+    // Supports weights 100-900
+    import '@fontsource-variable/roboto-condensed';
+    import '@fontsource-variable/Cabin';
+</script>
+
+
+<slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<h2> Olá meu chamo Henrique</h2>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+h1{
+    color: var(--feat);
+}
+
+</style>
