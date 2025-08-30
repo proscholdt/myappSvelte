938515d5c0c9dfa2d67d4d23b783e7d5f20238f2
Henrique Proscholdt <proscholdt.h2014@gmail.com>
2025-08-30 16:57:11 -0300

    Footer finalizado

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,40 @@
     import '@fontsource-variable/roboto-condensed';
     import '@fontsource-variable/Cabin';
     import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+	import Credits from '$lib/credits.svelte';
 </script>
 
-<Header/>
-<slot/>
+<div class="layout">
+
+    <Header/>
+    <main class="main">
+        <slot/>
+    </main>
+
+    <div class="footer__container">
+        <Footer/>
+    </div>
+
+    <div class="credits__container">
+        <Credits/>
+    </div>
+</div>
+
+<style>
+    .layout{
+        display:flex;
+        flex-direction:column;
+        height:100%
+    }
+
+    .footer__container{
+        margin-bottom:auto;
+    }
+
+    .credits__container{
+        margin-bottom:-70px;
+    }
+</style>
+
+
